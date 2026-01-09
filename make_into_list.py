c7ffa1cdc57fd543460f73eba4562f6914c5ac31
REJCU <ryan.ericson@my.jcu.edu.au>
2026-01-09 10:12:10 +1000

    prints random choice of words

--- make_into_list.py
+++ make_into_list.py
@@ -1,17 +1,15 @@
 import random
 
 
-FORMAT_TO_LIST = [""" place leave stop around mile very very its found men took number about earth the with does earth large for try now if paper must as may seem idea never high so be little
-fire what another many year both try must long own his but oil soon you know would got which began book near those will life watch idea try was ask in know began later
-get is follow out there feet river walk enough different come such every you with study these any way point show has """] 
+words_str = "place leave stop around mile very very its found men took number about earth the with does earth large for try now if paper must as may seem idea never high so be little fire what another many year both try must long own his but oil soon you know would got which began book near those will life watch idea try was ask in know began later get is follow out there feet river walk enough different come such every you with study these any way point show has" 
 
+def turn_list(x):
+    words = x.split()
+    quoted_string = ' ,'.join([f'"{word}"' for word in words])
 
-def turn_list(x): 
-    for i in x:
-        print(i.replace(" ", "\""))
+    print(quoted_string)
 
-
-turn_list(FORMAT_TO_LIST)
+turn_list(words_str)
 
 
 
---
+++ touch_type.py
@@ -0,0 +1,16 @@
+import random 
+
+WORDS = ["place" ,"leave" ,"stop" ,"around" ,"mile" ,"very" ,"very" ,"its" ,"found" ,"men" ,"took" ,"number" 
+         ,"about" ,"earth" ,"the" ,"with" ,"does" ,"earth" ,"large" ,"for" ,"try" ,"now" ,"if" ,"paper" ,"must" 
+         ,"as" ,"may" ,"seem" ,"idea" ,"never" ,"high" ,"so" ,"be" ,"little" ,"fire" ,"what" ,"another" ,"many" 
+         ,"year" ,"both" ,"try" ,"must" ,"long" ,"own" ,"his" ,"but" ,"oil" ,"soon" ,"you" ,"know" ,"would" ,"got" 
+         ,"which" ,"began" ,"book" ,"near" ,"those" ,"will" ,"life" ,"watch" ,"idea" ,"try" ,"was" ,"ask" ,"in" ,"know" 
+         ,"began" ,"later" ,"get" ,"is" ,"follow" ,"out" ,"there" ,"feet" ,"river" ,"walk" ,"enough" ,"different" ,"come" 
+         ,"such" ,"every" ,"you" ,"with" ,"study" ,"these" ,"any" ,"way" ,"point" ,"show" ,"has"]
+
+# for i in WORDS:
+  #  print(i)
+
+x = random.choice(WORDS)
+
+print(x)
