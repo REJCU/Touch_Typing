38f9b9978b9712eee2d7d99a0c14f250fc3933d2
REJCU <ryan.ericson@my.jcu.edu.au>
2026-01-10 15:41:17 +1000

    Completed basic rendition that prints out a number and checks if y in x

--- touch_type.py
+++ touch_type.py
@@ -1,16 +1,115 @@
-import random 
+import random
 
-WORDS = ["place" ,"leave" ,"stop" ,"around" ,"mile" ,"very" ,"very" ,"its" ,"found" ,"men" ,"took" ,"number" 
-         ,"about" ,"earth" ,"the" ,"with" ,"does" ,"earth" ,"large" ,"for" ,"try" ,"now" ,"if" ,"paper" ,"must" 
-         ,"as" ,"may" ,"seem" ,"idea" ,"never" ,"high" ,"so" ,"be" ,"little" ,"fire" ,"what" ,"another" ,"many" 
-         ,"year" ,"both" ,"try" ,"must" ,"long" ,"own" ,"his" ,"but" ,"oil" ,"soon" ,"you" ,"know" ,"would" ,"got" 
-         ,"which" ,"began" ,"book" ,"near" ,"those" ,"will" ,"life" ,"watch" ,"idea" ,"try" ,"was" ,"ask" ,"in" ,"know" 
-         ,"began" ,"later" ,"get" ,"is" ,"follow" ,"out" ,"there" ,"feet" ,"river" ,"walk" ,"enough" ,"different" ,"come" 
-         ,"such" ,"every" ,"you" ,"with" ,"study" ,"these" ,"any" ,"way" ,"point" ,"show" ,"has"]
+WORDS = [
+    "place",
+    "leave",
+    "stop",
+    "around",
+    "mile",
+    "very",
+    "very",
+    "its",
+    "found",
+    "men",
+    "took",
+    "number",
+    "about",
+    "earth",
+    "the",
+    "with",
+    "does",
+    "earth",
+    "large",
+    "for",
+    "try",
+    "now",
+    "if",
+    "paper",
+    "must",
+    "as",
+    "may",
+    "seem",
+    "idea",
+    "never",
+    "high",
+    "so",
+    "be",
+    "little",
+    "fire",
+    "what",
+    "another",
+    "many",
+    "year",
+    "both",
+    "try",
+    "must",
+    "long",
+    "own",
+    "his",
+    "but",
+    "oil",
+    "soon",
+    "you",
+    "know",
+    "would",
+    "got",
+    "which",
+    "began",
+    "book",
+    "near",
+    "those",
+    "will",
+    "life",
+    "watch",
+    "idea",
+    "try",
+    "was",
+    "ask",
+    "in",
+    "know",
+    "began",
+    "later",
+    "get",
+    "is",
+    "follow",
+    "out",
+    "there",
+    "feet",
+    "river",
+    "walk",
+    "enough",
+    "different",
+    "come",
+    "such",
+    "every",
+    "you",
+    "with",
+    "study",
+    "these",
+    "any",
+    "way",
+    "point",
+    "show",
+    "has",
+]
 
 # for i in WORDS:
-  #  print(i)
+#  print(i)
 
-x = random.choice(WORDS)
 
-print(x)
+scores = 0
+
+target = int(input("Repetition: "))
+
+
+while scores <= target:
+    x = random.choice(WORDS)
+    print(x)
+    y = input("Touch Type: ")
+
+    if y in x:
+        print("correct")
+        scores += 1
+    else:
+        print(x)
+        print("incorrect")
